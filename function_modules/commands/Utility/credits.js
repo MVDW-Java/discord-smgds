d1fb19fa3360b7ca20d89b65057114ccbc3e7c5d
MVDW <maxvdwolf@gmail.com>
2021-11-07 00:10:00 +0100

    Update credits.js

--- function_modules/commands/Utility/credits.js
+++ function_modules/commands/Utility/credits.js
@@ -22,6 +22,7 @@ module.exports = {
 			.addField("Avatar/Ping command:",":black_circle: @Yoshiko#7777", true)
 			.addField("Coinflip/Dice/Spoonful command:",":black_circle: @MVDW#0001", true)
 			.addField("Help/Credits command:",":black_circle: @MVDW#0001", true)
+			.addField("Music command:",":black_circle: @MVDW#0001\n:black_circle: @riidefi#2741", true)
 			.setColor("#2ecc71")
 			.setFooter("Requested by " + msg.author.tag + ".", msg.author.avatarURL())
 			.setTimestamp();
