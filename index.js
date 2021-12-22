1161f648cb66d6ca325eafc9acdf8c96f2c2466f
MVDW <maxvdwolf@gmail.com>
2021-12-22 21:00:56 +0100

    testing slash commands

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const imports = require('./imports');				// Global imports
 require('dotenv').config();
 
 const Discord = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const fs = require("fs");
 const path = require("path");
 
+
 const TOKEN = process.env.TOKEN;
+var command_list = [];
+
+
 
 // this is for cleaning up code and is called when the app exits or closes for any reason.
 var cleanup = require('./cleanup').Cleanup(myCleanup);
@@ -47,6 +54,12 @@ arrayOfFiles.forEach(function (file) {
 		
 		mods_loaded++;
 		process.stdout.write("   [ \x1b[32mOK\x1b[0m ] \n");
+		if(type_module == "command"){
+			var command_module = imports.ModuleImports.get(file).CommandName[0].toLowerCase();
+			const command_builder = new SlashCommandBuilder().setName(command_module).setDescription('MVDW TEST ON ' + command_module);
+			console.log(command_builder);
+			command_list.push(command_builder);
+		}
 
 	} catch (e) {
 		process.stdout.write("   [ \x1b[31mFAIL\x1b[0m ]\n");
@@ -68,11 +81,27 @@ fs.readdirSync(EventsNormalizedPath).forEach(function (file) {
 		process.stdout.write("\x1b[31mERROR: The module '" + file + "' will not be loaded in, please fix the issue.\x1b[0m\n");
 	}
 });
+
+console.log("Registering commands to server");
+const rest = new REST({ version: '9' }).setToken(TOKEN);
+
+
+
+rest.put(Routes.applicationCommands("830451181334954025"), { body: command_list })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
+
+
+
 if(mods_loaded == mods_total) {
 	console.log("\x1b[36m[" + mods_loaded + "/" + mods_total + "] \x1b[32mEverything loaded up, logging into servers...\x1b[0m\n");
 } else {
 	console.log("\x1b[36m[" + mods_loaded + "/" + mods_total + "] \x1b[33mWARNING: Not all modules seem to be loaded! We will try to continue but some things may be broken. logging into servers...\x1b[0m\n");
 }
 
+
+
+
+
 imports.bot.login(TOKEN);
-require('discord-buttons')(imports.bot)
+
